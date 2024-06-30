4a0e973009b467280dd110bed5577f2242d17ebc
alok2986 <115523299+alok2986@users.noreply.github.com>
2024-06-30 15:27:52 +0530

    Add files via upload

---
+++ SplashActivity.java
@@ -0,0 +1,28 @@
+package net.penguincoders.doit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.doit.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+        getSupportActionBar().hide();
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
